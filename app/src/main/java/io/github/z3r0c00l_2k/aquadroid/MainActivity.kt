64aad0a5cb17f0a42efd9115929168555d22bbe0
z3r0c00l-2k <mjack1327@gmail.com>
2019-05-14 21:11:15 +0530

    Implementing MainActivity IU

--- app/src/main/java/io/github/z3r0c00l_2k/aquadroid/MainActivity.kt
+++ app/src/main/java/io/github/z3r0c00l_2k/aquadroid/MainActivity.kt
@@ -1,17 +1,12 @@
 package io.github.z3r0c00l_2k.aquadroid
 
-import android.os.Build
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-        }
         setContentView(R.layout.activity_main)
     }
 }
