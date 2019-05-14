d4534c43e6864543282314e4a5f426e884b3508e
z3r0c00l-2k <mjack1327@gmail.com>
2019-05-14 14:05:08 +0530

    Git Init

---
+++ app/src/main/java/io/github/z3r0c00l_2k/aquadroid/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.z3r0c00l_2k.aquadroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
