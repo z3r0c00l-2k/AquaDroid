0561d14b67523c88b9c627b86dd37e4158a56a8c
z3r0c00l-2k <mjack1327@gmail.com>
2020-01-20 11:02:50 +0530

    adding custom intake option. and changing all intake values

--- app/src/main/java/io/github/z3r0c00l_2k/aquadroid/MainActivity.kt
+++ app/src/main/java/io/github/z3r0c00l_2k/aquadroid/MainActivity.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Handler
+import android.text.TextUtils
 import android.util.TypedValue
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.daimajia.androidanimations.library.Techniques
 import com.daimajia.androidanimations.library.YoYo
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 import io.github.z3r0c00l_2k.aquadroid.fragments.BottomSheetFragment
 import io.github.z3r0c00l_2k.aquadroid.helpers.AlarmHelper
 import io.github.z3r0c00l_2k.aquadroid.helpers.SqliteHelper
@@ -25,7 +29,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var dateNow: String
     private var notificStatus: Boolean = false
     private var selectedOption: Int? = null
-    private var selectedOptionName: String? = null
     private var snackbar: Snackbar? = null
     private var doubleBackToExitPressedOnce = false
 
@@ -55,13 +58,20 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
 
         val outValue = TypedValue()
-        applicationContext.theme.resolveAttribute(android.R.attr.selectableItemBackground, outValue, true)
+        applicationContext.theme.resolveAttribute(
+            android.R.attr.selectableItemBackground,
+            outValue,
+            true
+        )
 
         notificStatus = sharedPref.getBoolean(AppUtils.NOTIFICATION_STATUS_KEY, true)
         val alarm = AlarmHelper()
         if (!alarm.checkAlarm(this) && notificStatus) {
             btnNotific.setImageDrawable(getDrawable(R.drawable.ic_bell))
-            alarm.setAlarm(this, sharedPref.getInt(AppUtils.NOTIFICATION_FREQUENCY_KEY, 30).toLong())
+            alarm.setAlarm(
+                this,
+                sharedPref.getInt(AppUtils.NOTIFICATION_FREQUENCY_KEY, 30).toLong()
+            )
         }
 
         if (notificStatus) {
@@ -82,26 +92,27 @@ class MainActivity : AppCompatActivity() {
         }
 
         fabAdd.setOnClickListener {
-            if (selectedOption != null && selectedOptionName != null) {
+            if (selectedOption != null) {
                 if ((inTook * 100 / totalIntake) <= 140) {
                     if (sqliteHelper.addIntook(dateNow, selectedOption!!) > 0) {
                         inTook += selectedOption!!
                         setWaterLevel(inTook, totalIntake)
 
-                        Snackbar.make(it, "Your water intake was saved...!!", Snackbar.LENGTH_SHORT).show()
+                        Snackbar.make(it, "Your water intake was saved...!!", Snackbar.LENGTH_SHORT)
+                            .show()
 
                     }
                 } else {
                     Snackbar.make(it, "You already achieved the goal", Snackbar.LENGTH_SHORT).show()
                 }
                 selectedOption = null
-                selectedOptionName = null
-                opAqua.background = getDrawable(outValue.resourceId)
-                opCoffee.background = getDrawable(outValue.resourceId)
-                opTea.background = getDrawable(outValue.resourceId)
-                opSoftDrink.background = getDrawable(outValue.resourceId)
-                opJuice.background = getDrawable(outValue.resourceId)
-                opMilk.background = getDrawable(outValue.resourceId)
+                tvCustom.text = "Custom"
+                op50ml.background = getDrawable(outValue.resourceId)
+                op100ml.background = getDrawable(outValue.resourceId)
+                op150ml.background = getDrawable(outValue.resourceId)
+                op200ml.background = getDrawable(outValue.resourceId)
+                op250ml.background = getDrawable(outValue.resourceId)
+                opCustom.background = getDrawable(outValue.resourceId)
             } else {
                 YoYo.with(Techniques.Shake)
                     .duration(700)
@@ -116,7 +127,10 @@ class MainActivity : AppCompatActivity() {
             if (notificStatus) {
                 btnNotific.setImageDrawable(getDrawable(R.drawable.ic_bell))
                 Snackbar.make(it, "Notification Enabled..", Snackbar.LENGTH_SHORT).show()
-                alarm.setAlarm(this, sharedPref.getInt(AppUtils.NOTIFICATION_FREQUENCY_KEY, 30).toLong())
+                alarm.setAlarm(
+                    this,
+                    sharedPref.getInt(AppUtils.NOTIFICATION_FREQUENCY_KEY, 30).toLong()
+                )
             } else {
                 btnNotific.setImageDrawable(getDrawable(R.drawable.ic_bell_disabled))
                 Snackbar.make(it, "Notification Disabled..", Snackbar.LENGTH_SHORT).show()
@@ -129,93 +143,109 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        opAqua.setOnClickListener {
-            if (snackbar != null) {
-                snackbar?.dismiss()
-            }
-            selectedOption = 200
-            selectedOptionName = "Aqua"
-            opAqua.background = getDrawable(R.drawable.option_select_bg)
-            opCoffee.background = getDrawable(outValue.resourceId)
-            opTea.background = getDrawable(outValue.resourceId)
-            opSoftDrink.background = getDrawable(outValue.resourceId)
-            opJuice.background = getDrawable(outValue.resourceId)
-            opMilk.background = getDrawable(outValue.resourceId)
-
-        }
-
-        opCoffee.setOnClickListener {
-            if (snackbar != null) {
-                snackbar?.dismiss()
-            }
-            selectedOption = 150
-            selectedOptionName = "Coffee"
-            opAqua.background = getDrawable(outValue.resourceId)
-            opCoffee.background = getDrawable(R.drawable.option_select_bg)
-            opTea.background = getDrawable(outValue.resourceId)
-            opSoftDrink.background = getDrawable(outValue.resourceId)
-            opJuice.background = getDrawable(outValue.resourceId)
-            opMilk.background = getDrawable(outValue.resourceId)
-
-        }
-
-        opTea.setOnClickListener {
-            if (snackbar != null) {
-                snackbar?.dismiss()
-            }
-            selectedOption = 150
-            selectedOptionName = "Tea"
-            opAqua.background = getDrawable(outValue.resourceId)
-            opCoffee.background = getDrawable(outValue.resourceId)
-            opTea.background = getDrawable(R.drawable.option_select_bg)
-            opSoftDrink.background = getDrawable(outValue.resourceId)
-            opJuice.background = getDrawable(outValue.resourceId)
-            opMilk.background = getDrawable(outValue.resourceId)
-
-        }
-
-        opSoftDrink.setOnClickListener {
-            if (snackbar != null) {
-                snackbar?.dismiss()
-            }
-            selectedOption = 200
-            selectedOptionName = "Soft Drink"
-            opAqua.background = getDrawable(outValue.resourceId)
-            opCoffee.background = getDrawable(outValue.resourceId)
-            opTea.background = getDrawable(outValue.resourceId)
-            opSoftDrink.background = getDrawable(R.drawable.option_select_bg)
-            opJuice.background = getDrawable(outValue.resourceId)
-            opMilk.background = getDrawable(outValue.resourceId)
-
-        }
-
-        opJuice.setOnClickListener {
-            if (snackbar != null) {
-                snackbar?.dismiss()
-            }
-            selectedOption = 200
-            selectedOptionName = "Juice"
-            opAqua.background = getDrawable(outValue.resourceId)
-            opCoffee.background = getDrawable(outValue.resourceId)
-            opTea.background = getDrawable(outValue.resourceId)
-            opSoftDrink.background = getDrawable(outValue.resourceId)
-            opJuice.background = getDrawable(R.drawable.option_select_bg)
-            opMilk.background = getDrawable(outValue.resourceId)
-
-        }
-
-        opMilk.setOnClickListener {
+        op50ml.setOnClickListener {
             if (snackbar != null) {
                 snackbar?.dismiss()
             }
             selectedOption = 50
-            selectedOptionName = "Milk"
-            opAqua.background = getDrawable(outValue.resourceId)
-            opCoffee.background = getDrawable(outValue.resourceId)
-            opTea.background = getDrawable(outValue.resourceId)
-            opSoftDrink.background = getDrawable(outValue.resourceId)
-            opJuice.background = getDrawable(outValue.resourceId)
-            opMilk.background = getDrawable(R.drawable.option_select_bg)
+            op50ml.background = getDrawable(R.drawable.option_select_bg)
+            op100ml.background = getDrawable(outValue.resourceId)
+            op150ml.background = getDrawable(outValue.resourceId)
+            op200ml.background = getDrawable(outValue.resourceId)
+            op250ml.background = getDrawable(outValue.resourceId)
+            opCustom.background = getDrawable(outValue.resourceId)
+
+        }
+
+        op100ml.setOnClickListener {
+            if (snackbar != null) {
+                snackbar?.dismiss()
+            }
+            selectedOption = 100
+            op50ml.background = getDrawable(outValue.resourceId)
+            op100ml.background = getDrawable(R.drawable.option_select_bg)
+            op150ml.background = getDrawable(outValue.resourceId)
+            op200ml.background = getDrawable(outValue.resourceId)
+            op250ml.background = getDrawable(outValue.resourceId)
+            opCustom.background = getDrawable(outValue.resourceId)
+
+        }
+
+        op150ml.setOnClickListener {
+            if (snackbar != null) {
+                snackbar?.dismiss()
+            }
+            selectedOption = 150
+            op50ml.background = getDrawable(outValue.resourceId)
+            op100ml.background = getDrawable(outValue.resourceId)
+            op150ml.background = getDrawable(R.drawable.option_select_bg)
+            op200ml.background = getDrawable(outValue.resourceId)
+            op250ml.background = getDrawable(outValue.resourceId)
+            opCustom.background = getDrawable(outValue.resourceId)
+
+        }
+
+        op200ml.setOnClickListener {
+            if (snackbar != null) {
+                snackbar?.dismiss()
+            }
+            selectedOption = 200
+            op50ml.background = getDrawable(outValue.resourceId)
+            op100ml.background = getDrawable(outValue.resourceId)
+            op150ml.background = getDrawable(outValue.resourceId)
+            op200ml.background = getDrawable(R.drawable.option_select_bg)
+            op250ml.background = getDrawable(outValue.resourceId)
+            opCustom.background = getDrawable(outValue.resourceId)
+
+        }
+
+        op250ml.setOnClickListener {
+            if (snackbar != null) {
+                snackbar?.dismiss()
+            }
+            selectedOption = 250
+            op50ml.background = getDrawable(outValue.resourceId)
+            op100ml.background = getDrawable(outValue.resourceId)
+            op150ml.background = getDrawable(outValue.resourceId)
+            op200ml.background = getDrawable(outValue.resourceId)
+            op250ml.background = getDrawable(R.drawable.option_select_bg)
+            opCustom.background = getDrawable(outValue.resourceId)
+
+        }
+
+        opCustom.setOnClickListener {
+            if (snackbar != null) {
+                snackbar?.dismiss()
+            }
+
+            val li = LayoutInflater.from(this)
+            val promptsView = li.inflate(R.layout.custom_input_dialog, null)
+
+            val alertDialogBuilder = AlertDialog.Builder(this)
+            alertDialogBuilder.setView(promptsView)
+
+            val userInput = promptsView
+                .findViewById(R.id.etCustomInput) as TextInputLayout
+
+            alertDialogBuilder.setPositiveButton("OK") { dialog, id ->
+                val inputText = userInput.editText!!.text.toString()
+                if (!TextUtils.isEmpty(inputText)) {
+                    tvCustom.text = "${inputText} ml"
+                    selectedOption = inputText.toInt()
+                }
+            }.setNegativeButton("Cancel") { dialog, id ->
+                dialog.cancel()
+            }
+
+            val alertDialog = alertDialogBuilder.create()
+            alertDialog.show()
+
+            op50ml.background = getDrawable(outValue.resourceId)
+            op100ml.background = getDrawable(outValue.resourceId)
+            op150ml.background = getDrawable(outValue.resourceId)
+            op200ml.background = getDrawable(outValue.resourceId)
+            op250ml.background = getDrawable(outValue.resourceId)
+            opCustom.background = getDrawable(R.drawable.option_select_bg)
 
         }
 
@@ -235,7 +265,8 @@ class MainActivity : AppCompatActivity() {
             .playOn(intakeProgress)
         intakeProgress.currentProgress = progress
         if ((inTook * 100 / totalIntake) > 140) {
-            Snackbar.make(main_activity_parent, "You achieved the goal", Snackbar.LENGTH_SHORT).show()
+            Snackbar.make(main_activity_parent, "You achieved the goal", Snackbar.LENGTH_SHORT)
+                .show()
         }
     }
 
